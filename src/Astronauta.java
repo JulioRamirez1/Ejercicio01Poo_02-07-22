de7143d70907fb9a8a81f77953aab80cb7db4ff5
Julio <41455450+JulioRamirez1@users.noreply.github.com>
2022-07-02 12:59:33 -0500

    Add files via upload

---
+++ src/Astronauta.java
@@ -0,0 +1,26 @@
+
+public class Astronauta {
+	private int cantidadOxigeno;
+	private String observarPlaneta;
+	private String agujeroNegro;
+	
+	public int getCantidadOxigeno() {
+		return cantidadOxigeno;
+	}
+	public void setCantidadOxigeno(int cantidadOxigeno) {
+		this.cantidadOxigeno = cantidadOxigeno;
+	}
+	public String getObservarPlaneta() {
+		return observarPlaneta;
+	}
+	public void setObservarPlaneta(String observarPlaneta) {
+		this.observarPlaneta = observarPlaneta;
+	}
+	public String getAgujeroNegro() {
+		return agujeroNegro;
+	}
+	public void setAgujeroNegro(String agujeroNegro) {
+		this.agujeroNegro = agujeroNegro;
+	}
+	
+}
---
+++ src/PooEjercicio01.java
@@ -0,0 +1,12 @@
+
+public class PooEjercicio01 {
+	
+	public static void main(String[] args) {
+		Astronauta astronauta = new Astronauta();
+		astronauta.setCantidadOxigeno(5);
+		System.out.print("Observe un planeta,");
+		System.out.print("Nos absorbe un agujero negro, Corran! ");
+		System.out.print("Y tengo " + astronauta.getCantidadOxigeno() + " de oxigeno");
+	}
+	
+}
